c8ead4a5d534879f56e03175ef618da3b56b8b71
Yamashi <yamashi@live.com>
2012-11-13 22:47:09 +0100

    - Protection in case the GOMDatabase does not contain the GOMServer

--- Src/Game/PlayerHandlers.cpp
+++ Src/Game/PlayerHandlers.cpp
@@ -1,6 +1,7 @@
 #include "Player.hpp"
 #include <System/Log.h>
 #include <Game/MassiveMessageManager.hpp>
+#include <System/Tools.h>
 
 namespace Game
 {
@@ -21,10 +22,11 @@ namespace Game
 			for(auto itor = states.begin(), end = states.end(); itor != end; ++itor)
 			{
 				IGOMServer* server = TheMassiveMessageMgr->GetGOMDatabase()->Get(itor->first);
-				os << "\tGroup : " << server->GetGroup() << std::endl;
+				os << "\tGroup : " << Framework::System::IntToString(itor->first) << std::endl;
 				for(auto itor2 = itor->second.begin(), end2 = itor->second.end(); itor2 != end2; ++itor2)
 				{
-					server->UpdateEntry(itor2->id, itor2->state, itor2->data);
+					if(server)
+						server->UpdateEntry(itor2->id, itor2->state, itor2->data);
 					os << "\t\tId: " << itor2->id << " , State : " << itor2->state << std::endl;
 				}
 			}
@@ -38,18 +40,21 @@ namespace Game
 			for(auto itor = deleted.begin(), end = deleted.end(); itor != end; ++itor)
 			{
 				IGOMServer* server = TheMassiveMessageMgr->GetGOMDatabase()->Get(itor->first);
-				os << "\tGroup : " << server->GetGroup() << std::endl;
+				os << "\tGroup : " << Framework::System::IntToString(itor->first) << std::endl;
 				for(auto itor2 = itor->second.begin(), end2 = itor->second.end(); itor2 != end2; ++itor2)
 				{
-					server->Remove(kTransactionFull, *itor2);
-					server->Remove(kTransactionPartial, *itor2);
-					server->Remove(kTransactionNone, *itor2);
-
+					if(server)
+					{
+						server->Remove(kTransactionFull, *itor2);
+						server->Remove(kTransactionPartial, *itor2);
+						server->Remove(kTransactionNone, *itor2);
+					}
+					
 					os << "\t\tId: " << *itor2 << std::endl;
 				}
 			}
 		}
-		//Framework::System::Log::Print(os.str());
+		Framework::System::Log::Print(os.str());
 	}
 
 	void Player::HandleAwareness(Framework::Network::Packet& pPacket)
